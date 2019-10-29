e6c87d3d2d43ac0735e71e40010127f7d8e0fbb7
niclin <bboyceo@hotmail.com>
2019-10-29 22:15:46 +0800

    Add scaffold

---
+++ spec/views/users/index.html.erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "users/index", type: :view do
+  before(:each) do
+    assign(:users, [
+      User.create!(
+        :name => "Name",
+        :email => "Email"
+      ),
+      User.create!(
+        :name => "Name",
+        :email => "Email"
+      )
+    ])
+  end
+
+  it "renders a list of users" do
+    render
+    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => "Email".to_s, :count => 2
+  end
+end
---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "users/show", type: :view do
+  before(:each) do
+    @user = assign(:user, User.create!(
+      :name => "Name",
+      :email => "Email"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Email/)
+  end
+end
